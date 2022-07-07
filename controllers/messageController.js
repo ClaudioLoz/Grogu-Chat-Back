311d5a0157ac226dd83e0d9588feea5c2201ec44
ClaudioLoz <a20180472@pucp.edu.pe>
2022-07-07 04:00:09 -0500

    initializing

---
+++ controllers/messageController.js
@@ -0,0 +1,40 @@
+const Messages = require("../models/messageModel");
+
+module.exports.addMessage = async (req, res, next) => {
+  try {
+    const { from, to, message } = req.body;
+    const data = await Messages.create({
+      message: { text: message },
+      users: [from, to],
+      sender: from,
+    });
+
+    if (data) return res.json({ msg: "Mensaje añadido correctamente" });
+    else return res.json({ msg: "No se pudo agregar el mensaje a la base de datos" });
+  } catch (ex) {
+    next(ex);
+  }
+};
+
+module.exports.getMessages = async (req, res, next) => {
+    try {
+      const { from, to } = req.body;
+  
+      const messages = await Messages.find({
+        users: {
+          $all: [from, to],
+        },
+      }).sort({ updatedAt: 1 });
+  
+      const projectedMessages = messages.map((msg) => {
+        return {
+          fromSelf: msg.sender.toString() === from,
+          message: msg.message.text,
+        };
+      });
+      res.json(projectedMessages);
+    } catch (ex) {
+      next(ex);
+    }
+  };
+  
