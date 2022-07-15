442de82a74bf47886ff10f028801784187568125
Claudio Lozano <70769241+ClaudioLoz@users.noreply.github.com>
2022-07-15 12:34:07 -0500

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
-//my db: mongodb+srv://ClaudioLoz:Jersyselacome2022@cluster-0.sgzxq.mongodb.net/?retryWrites=true&w=majority
+
 mongoose
   .connect(process.env.MONGO_URL, {
     useNewUrlParser: true,
